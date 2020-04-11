581c6da3f07c1627e11cb74e2bd524d56f28d3e0
santhoshv456 <santhoshv456@gmail.com>
2020-04-11 16:15:34 +0530

    End of section 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/PreventUnsavedChanges';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 
 
@@ -57,7 +58,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MembersCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
    ],
    imports: [
       BrowserModule,
---
+++ DatingApp.API/Helpers/LogUserActivity.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security;
+using System.Security.Claims;
+using DatingApp.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DatingApp.API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var reslutContext= await next();
+            var userId = int.Parse(reslutContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var _repo = reslutContext.HttpContext.RequestServices.GetService<IDatingRepository>();
+            var user =await _repo.GetUser(userId);
+            user.LastActive = DateTime.Now;
+            await _repo.SaveAll();
+        }
+    }
+}
